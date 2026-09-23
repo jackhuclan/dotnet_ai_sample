0f20b4deac68e956e2b30903e1111dca4210e646
hujinping <hujinping@megarobo.com>
2026-09-23 15:17:46 +0800

    Agent_OpenAI_Step02_Reasoning

--- dotnet_ai_agent_sample/Agent_OpenAI_Step02_Reasoning.cs
+++ dotnet_ai_agent_sample/Agent_OpenAI_Step02_Reasoning.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Agents.AI;
 using Microsoft.Extensions.AI;
 using OpenAI;
+using OpenAI.Chat;
 using System.ClientModel;
 
 namespace dotnet_ai_agent_sample;
@@ -35,6 +36,42 @@ public class Agent_OpenAI_Step02_Reasoning
 
         Console.WriteLine("Token usage:");
         Console.WriteLine($"Input: {response.Usage?.InputTokenCount}, Output: {response.Usage?.OutputTokenCount}, {string.Join(", ", response.Usage?.AdditionalCounts ?? [])}");
+        Console.WriteLine();
+
+        Console.WriteLine("2. Streaming");
+        await foreach (var update in agent.RunStreamingAsync("用简单的话解释相对论。"))
+        {
+            foreach (var item in update.Contents)
+            {
+                if (item is TextReasoningContent reasoningContent)
+                {
+                    Console.Write($"\e[38;2;255;165;0m{reasoningContent.Text}\e[0m");
+                    //Console.Write($"\e[97m{reasoningContent.Text}\e[0m");
+                }
+                else if (item is TextContent textContent)
+                {
+                    Console.Write(textContent.Text);
+                }
+            }
+        }
+    }
+
+    public async Task Run2()
+    {
+        var envSetting = new EnvSetting();
+
+        AIAgent agent = new OpenAIClient(new ApiKeyCredential(envSetting.OPENAI_API_KEY), new OpenAIClientOptions
+        {
+            Endpoint = new Uri("https://api.deepseek.com"),
+        }).GetChatClient(envSetting.OPENAI_CHAT_MODEL_NAME).AsAIAgent();
+
+        Console.WriteLine("1. Non-streaming:");
+        var response = await agent.RunAsync("解决下面的问题：如果一辆火车以每小时60英里的速度行驶，需要行驶180英里，那么行程需要多长时间？请展示你的推理过程。");
+
+        Console.WriteLine(response.Text);
+
+        Console.WriteLine("Token usage:");
+        Console.WriteLine($"Input: {response.Usage?.InputTokenCount}, Output: {response.Usage?.OutputTokenCount}, {string.Join(", ", response.Usage?.AdditionalCounts ?? [])}");
         //Console.WriteLine();
 
         Console.WriteLine("2. Streaming");
@@ -44,7 +81,8 @@ public class Agent_OpenAI_Step02_Reasoning
             {
                 if (item is TextReasoningContent reasoningContent)
                 {
-                    Console.Write($"\e[97m{reasoningContent.Text}\e[0m");
+                    Console.Write($"\e[38;2;255;165;0m{reasoningContent.Text}\e[0m");
+                    //Console.Write($"\e[97m{reasoningContent.Text}\e[0m");
                 }
                 else if (item is TextContent textContent)
                 {
--- dotnet_ai_agent_sample/Program.cs
+++ dotnet_ai_agent_sample/Program.cs
@@ -5,12 +5,12 @@ public class Program
     public static async Task Main(string[] args)
     {
         //await new Agent_OpenAI_Step01_Running().Run();
-        //await new Agent_OpenAI_Step02_Reasoning().Run();
+        await new Agent_OpenAI_Step02_Reasoning().Run();
         //await new Agent_OpenAI_Step03_CreateFromChatClient().Run();
         //await new Agent_OpenAI_Step04_CreateFromOpenAIResponseClient().Run();
         //await new Agent_OpenAI_Step05_Conversation().Run();
         //await new Agent_OpenAI_Step06_CodeInterpreterFileDownload().Run();
 
-        await new Agent_Step21_WebSearch().Run();
+        //await new Agent_Step21_WebSearch().Run();
     }
 }
